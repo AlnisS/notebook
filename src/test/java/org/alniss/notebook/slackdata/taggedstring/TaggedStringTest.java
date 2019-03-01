2b415447b0d9dfdeb620db3aa38b8f1393d7214d
AlnisS <alnis.smidchens@gmail.com>
2019-02-28 20:45:06 -0800

    test stuff, might be broken

---
+++ src/test/java/org/alniss/notebook/slackdata/taggedstring/TaggedStringTest.java
@@ -0,0 +1,13 @@
+package org.alniss.notebook.slackdata.taggedstring;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TaggedStringTest {
+
+    @Test
+    public void initTagValues() {
+
+    }
+}
